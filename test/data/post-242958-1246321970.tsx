529e973b4c93a6507431b0da85695320f353c349
nicolas marmot <nicolasmarmot@gmail.com>
2019-02-02 22:54:13 +0100

    map building

---
+++ test/data/post-242958-1246321970.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="post-242958-1246321970" tilewidth="16" tileheight="16" tilecount="2709" columns="63">
+ <image source="post-242958-1246321970.jpg" width="1008" height="689"/>
+</tileset>
---
+++ test/data/zelda01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="zelda01" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="zelda01.png" width="480" height="256"/>
+</tileset>
