605eb646516e4dd133e356e7b76e8b18ae0e0ba7
manu810 <emmanuel.julia810@gmail.com>
2019-02-13 15:02:26 +0100

    Commit Manu 3

---
+++ data/zelda_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="zelda_house" tilewidth="16" tileheight="16" tilecount="289" columns="17">
+ <image source="MC_House.png" width="272" height="272"/>
+</tileset>
