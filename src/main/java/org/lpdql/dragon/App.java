34ceee6f08b3cc57c236004e403f3248993c7a04
Assani <ambi9.3@outlook.fr>
2019-04-03 16:41:24 +0200

    test des niveaux et expériences

--- src/main/java/org/lpdql/dragon/App.java
+++ src/main/java/org/lpdql/dragon/App.java
@@ -35,6 +35,7 @@ public class App {
         System.setProperty(LIBRARY_PATH, javaLibraryPath + File.pathSeparatorChar + dir.getAbsolutePath());
 
         resetJavaLibraryPath();
+        
     }
     
     /**
