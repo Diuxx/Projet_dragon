d23cd7c08672f6e0b80cc057ad21b6d911f0dcb8
Diuxx <nicolasmarmot@gmail.com>
2019-04-08 01:34:58 +0100

    comment & tests

---
+++ data/map/minish_chip_inside_03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="minish_chip_inside_03" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="tiles/minish_chip_inside_03.png" width="480" height="256"/>
+</tileset>
