4bac3f44dd3eeb33650a63153359bae8ebec2891
Diuxx <nicolasmarmot@gmail.com>
2019-04-18 09:35:58 +0100

    fin de la bataille + ajout de sons

--- src/test/java/org/lpdql/dragon/AppTest.java
+++ src/test/java/org/lpdql/dragon/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.lpdql.dragon.jeu.MessageTest;
 import org.lpdql.dragon.jeu.TestLevelExperience;
 import org.lpdql.dragon.personnages.EnnemiTest;
+import org.lpdql.dragon.personnages.PersonnageNonJoueurTest;
 import org.lpdql.dragon.scenario.ScenarioTest;
 
 @RunWith(Suite.class)
@@ -13,6 +14,7 @@ import org.lpdql.dragon.scenario.ScenarioTest;
         EnnemiTest.class,
         TestLevelExperience.class,
         MessageTest.class,
+        PersonnageNonJoueurTest.class,
 })
 public class AppTest {
 
